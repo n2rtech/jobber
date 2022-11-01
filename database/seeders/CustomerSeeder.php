11333808a9ff885a080a2c21088b9f0e58fe6a5a
unknown <rc21292@gmail.com>
2022-11-01 19:40:18 +0530

    uploading all files

--- database/seeders/CustomerSeeder.php
+++ database/seeders/CustomerSeeder.php
@@ -25,13 +25,33 @@ class CustomerSeeder extends Seeder
             'mobile_1_name' => 'Prashant',
             'mobile_2' => '9555108968',
             'mobile_2_name' => 'Kenny',
+            'address_1' => 'C-6, Sector 7',
+            'address_2' => 'Noida',
+            'city' => 'GB Nagar',
+            'state' => 'Uttar Pradesh',
+            'country' => 'India',
+            'eir_code' => '110009',
+            'notes'     => 'Date:10-30-2022 10:05:00 Note:Note 1 inserted here',
+            'status' => 'completed'
+        ]);
+
+        Customer::create([
+            'id' => 1,
+            'name' => 'Kenny D',
+            'email' => 'info@daltontv.ie',
+            'phone' => '8747856988',
+            'phone_name' => 'Home',
+            'mobile_1' => '9876543210',
+            'mobile_1_name' => 'Mobile',
+            'mobile_2' => '975312648',
+            'mobile_2_name' => 'Office',
             'address_1' => 'Kilkenny Commercial Centre',
             'address_2' => 'Sion Road',
             'city' => 'Kilkenny',
             'state' => 'Kilkenny',
             'country' => 'Ireland',
             'eir_code' => 'LOC8',
-            'notes'     => 'abcd',
+            'notes'     => 'Date:10-30-2022 10:05:00 Note:Note 1 inserted here',
             'status' => 'completed'
         ]);
 
