df01e248b9f28a234af97e1221f3f31e94c5e4ee
Nurul Hasan <nhasan@n2rtechnologies.com>
2022-11-23 12:34:49 +0530

    CI/CD

--- deploy.php
+++ deploy.php
@@ -2,10 +2,13 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'contrib/php-fpm.php';
+require 'contrib/npm.php';
 
 // Config
 
 set('repository', 'git@gitlab.n2rtechnologies.com:rc21292/daltontvcrm.git');
+set('php_fpm_version', '8.0');
 
 add('shared_files', []);
 add('shared_dirs', []);
@@ -16,6 +19,24 @@ add('writable_dirs', []);
 host('216.158.229.197')
     ->set('remote_user', 'root')
     ->set('deploy_path', '/var/www/daltontvcrm');
+
+task('deploy', [
+    'deploy:prepare',
+    'deploy:vendors',
+    'artisan:storage:link',
+    'artisan:view:cache',
+    'artisan:config:cache',
+    'artisan:migrate',
+    'npm:install',
+    'npm:run:prod',
+    'deploy:publish',
+    'php-fpm:reload',
+]);
+
+task('npm:run:prod', function () {
+    cd('{{release_or_current_path}}');
+    run('npm run prod');
+});
 
 // Hooks
 
