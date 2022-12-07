302e44ab7323d3d72c1a5f6cbc525c8902b94cd2
unknown <rc21292@gmail.com>
2022-12-07 15:26:59 +0530

    uploading all files

--- app/Http/Controllers/ExternalFormController.php
+++ app/Http/Controllers/ExternalFormController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExternalForm;
+use App\Models\ExternalFormQuestion;
+use App\Models\ExternalFormQuestionOption;
+use App\Models\ExternalFormTab;
 use Illuminate\Http\Request;
 
 class ExternalFormController extends Controller
@@ -13,7 +17,8 @@ class ExternalFormController extends Controller
      */
     public function index()
     {
-        //
+        $external_forms = ExternalForm::get();
+        return view('settings.external-form.index', compact('external_forms'));
     }
 
     /**
@@ -56,7 +61,8 @@ class ExternalFormController extends Controller
      */
     public function edit($id)
     {
-        //
+        $external_form = ExternalForm::find($id);
+        return view('settings.external-form.edit', compact('external_form'));
     }
 
     /**
@@ -68,7 +74,78 @@ class ExternalFormController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $externalform = ExternalForm::find($id);
+
+        if($externalform->type == 'single'){
+            ExternalFormQuestion::where('external_form_id', $id)->delete();
+            ExternalFormQuestionOption::where('external_form_id', $id)->delete();
+            if(!empty($request->question && is_array($request->question))){
+
+                foreach($request->question as $key => $value){
+
+                    $question                       = new ExternalFormQuestion();
+                    $question->external_form_id     = $externalform->id;
+                    $question->question             = $value['question'];
+                    $question->type                 = $value['type'];
+                    $question->save();
+
+                    if(isset($value['option']) && !empty($value['option'] && is_array($value['option']))){
+
+                        foreach($value['option'] as $newkey => $optionvalue){
+
+                            $option                                 = new ExternalFormQuestionOption();
+                            $option->external_form_id               = $externalform->id;
+                            $option->external_form_question_id      = $question->id;
+                            $option->option                         = $optionvalue;
+                            $option->save();
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }else{
+            $externalformtab = ExternalFormTab::find($request->tab_id);
+            $externalformtab->tab = $request->tab;
+            $externalformtab->save();
+
+            ExternalFormQuestion::where('external_form_tab_id', $request->tab_id)->delete();
+            ExternalFormQuestionOption::where('external_form_tab_id', $request->tab_id)->delete();
+            if(!empty($request->question && is_array($request->question))){
+
+                foreach($request->question as $key => $value){
+
+                    $question                       = new ExternalFormQuestion();
+                    $question->external_form_id     = $externalform->id;
+                    $question->external_form_tab_id = $request->tab_id;
+                    $question->question             = $value['question'];
+                    $question->type                 = $value['type'];
+                    $question->save();
+
+                    if(isset($value['option']) && !empty($value['option'] && is_array($value['option']))){
+
+                        foreach($value['option'] as $newkey => $optionvalue){
+
+                            $option                                 = new ExternalFormQuestionOption();
+                            $option->external_form_id               = $externalform->id;
+                            $option->external_form_tab_id           = $request->tab_id;
+                            $option->external_form_question_id      = $question->id;
+                            $option->option                         = $optionvalue;
+                            $option->save();
+
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+
+        return redirect()->route('external-forms.index')->with('success', 'External Form Updated Successfully !');
     }
 
     /**
