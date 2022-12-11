c3c561a1d282cbc65d492d3e3783c1a62f444348
Nurul Hasan <nhasan@n2rtechnologies.com>
2022-12-11 21:17:40 +0530

    Move email logo to HTTPS

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -238,7 +238,8 @@ if (!function_exists('getCompanyAddress')) {
            $message     = str_replace('{{COMPANY_ADDRESS}}', getCompanyAddress($company->id) , $message);
            $message     = str_replace('{{COMPANY_VAT}}', $company->vat , $message);
            $message     = str_replace('{{COMPANY_WEBSITE}}', $company->website , $message);
-           $path        = asset('dist/img/logo-dark.png');
+           //$path        = asset('dist/img/logo-dark.png');
+           $path        = 'https://dev.n2rtechnologies.com/logo-dark.png';
            $message     = str_replace('{{COMPANY_LOGO}}', '<img src="'.$path.'" width="26%"></img>' , $message);
 
            $message     = str_replace('{{CUSTOMER_NAME}}', $job->customer->name , $message);
@@ -305,7 +306,8 @@ if (!function_exists('getCompanyAddress')) {
            $message     = str_replace('{{COMPANY_ADDRESS}}', getCompanyAddress($company->id) , $message);
            $message     = str_replace('{{COMPANY_VAT}}', $company->vat , $message);
            $message     = str_replace('{{COMPANY_WEBSITE}}', $company->website , $message);
-           $path        = asset('dist/img/logo-dark.png');
+           //$path        = asset('dist/img/logo-dark.png');
+           $path        = 'https://dev.n2rtechnologies.com/logo-dark.png';
            $message     = str_replace('{{COMPANY_LOGO}}', '<img src="'.$path.'" width="26%"></img>' , $message);
 
            $message     = str_replace('{{CUSTOMER_NAME}}', $estimate->customer->name , $message);
@@ -380,7 +382,8 @@ if (!function_exists('getCompanyAddress')) {
            $message     = str_replace('{{COMPANY_ADDRESS}}', getCompanyAddress($company->id) , $message);
            $message     = str_replace('{{COMPANY_VAT}}', $company->vat , $message);
            $message     = str_replace('{{COMPANY_WEBSITE}}', $company->website , $message);
-           $path        = asset('dist/img/logo-dark.png');
+           //$path        = asset('dist/img/logo-dark.png');
+           $path        = 'https://dev.n2rtechnologies.com/logo-dark.png';
            $message     = str_replace('{{COMPANY_LOGO}}', '<img src="'.$path.'" width="26%"></img>' , $message);
 
            $message     = str_replace('{{CUSTOMER_NAME}}', $invoice->customer->name , $message);
